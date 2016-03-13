0a62712c5cb186139b7c71d915871fb308abb134
Fabien Crespel <fabien@crespel.net>
2016-03-13 18:21:08 +0100

    Maven2: minor packaging improvements

--- system/maven2/src/main/scripts/postinstall.sh
+++ system/maven2/src/main/scripts/postinstall.sh
@@ -1,14 +1,17 @@
 interpolatetemplate_inplace "@{package.app}/conf/settings.xml"
 
 # Fix data directory permissions
-mkdir -p "@{product.data}/system/maven/repository"
-chown -R @{product.user}:@{product.group} "@{product.data}/system/maven/repository"
-chmod -R ug=rwX "@{product.data}/system/maven/repository"
+mkdir -p "@{package.data}/repository"
+chown -R @{product.user}:@{product.group} "@{package.data}/repository"
+chmod -R ug=rwX "@{package.data}/repository"
+
+# Create trust store if necessary
+create_truststore "@{package.data}/trust.jks" @{product.user} @{product.group} "@{system.java7.app}/jre/lib/security/cacerts"
 
 # Create link to repository in user home
 if [ ! -e "@{product.root}/.m2/repository" -a ! -h "@{product.root}/.m2/repository" ]; then
-	mkdir -p "@{product.root}/.m2" && chown @{package.user}:@{package.group} "@{product.root}/.m2"
-	ln -s "@{product.data}/system/maven/repository" "@{product.root}/.m2/repository" && chown -h @{package.user}:@{package.group} "@{product.root}/.m2/repository"
+	mkdir -p "@{product.root}/.m2" && chown @{product.user}:@{product.group} "@{product.root}/.m2"
+	ln -s "@{package.data}/repository" "@{product.root}/.m2/repository" && chown -h @{product.user}:@{product.group} "@{product.root}/.m2/repository"
 fi
 
 # Fix executable permissions
