15c111baf6e7880170a9a44f84d9fc9552239bad
Fabien Crespel <fabien@crespel.net>
2016-06-19 16:24:12 +0200

    Python: integration of mod_wsgi 4.5.2 (refs #5186)

--- system/python/src/main/scripts/postinstall.sh
+++ system/python/src/main/scripts/postinstall.sh
@@ -1,1 +1,26 @@
+APXS_LIBEXECDIR="@{product.app}/system/httpd/modules"
+MOD_WSGI_DIR="$APXS_LIBEXECDIR/mod_wsgi"
+
+# Fix script permissions
+chmod +x "$MOD_WSGI_DIR/configure"
+
+# Make and install mod_wsgi
+POSTINSTALL_OUTPUT=`mktemp --tmpdir=$PRODUCT_TMP`
+( cd "$MOD_WSGI_DIR" && make distclean ) > /dev/null 2>&1
+if ! ( cd "$MOD_WSGI_DIR" && ./configure && make ) > "$POSTINSTALL_OUTPUT" 2>&1; then
+	cat "$POSTINSTALL_OUTPUT"
+	rm -f "$POSTINSTALL_OUTPUT"
+	printerror "ERROR: failed to compile mod_wsgi"
+	exit 1
+fi
+if ! ( cd "$MOD_WSGI_DIR" && apxs -i -S LIBEXECDIR="$APXS_LIBEXECDIR" -n mod_wsgi src/server/mod_wsgi.la ) > "$POSTINSTALL_OUTPUT" 2>&1; then
+	cat "$POSTINSTALL_OUTPUT"
+	rm -f "$POSTINSTALL_OUTPUT"
+	printerror "ERROR: failed to install mod_wsgi"
+	exit 1
+fi
+( cd "$MOD_WSGI_DIR" && make distclean ) > /dev/null 2>&1
+rm -f "$POSTINSTALL_OUTPUT"
+
+# Reload HTTPD if already running
 reloadservice @{httpd.service}
