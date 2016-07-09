027263005ca4b09994d340fcf106f49591629dd8
Fabien Crespel <fabien@crespel.net>
2016-07-10 00:52:15 +0200

    Ruby: update to v2.2.5 (refs #5235) and Passenger 5 (refs #4536)

--- system/ruby/src/main/product/bin/profile.d/ruby.sh
+++ system/ruby/src/main/product/bin/profile.d/ruby.sh
@@ -6,6 +6,7 @@
 
 export RAILS_ENV="production"
 export PASSENGER_TMPDIR="@{product.tmp}"
+export PASSENGER_INSTANCE_REGISTRY_DIR="$PASSENGER_TMPDIR"
 
 export rvm_path="@{package.app}/rvm"
 if [ -e "$rvm_path/scripts/rvm" ]; then
