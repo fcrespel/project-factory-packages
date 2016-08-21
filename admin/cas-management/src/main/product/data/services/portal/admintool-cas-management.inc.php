4f2d37422da5af44e4a7ad49e3d669271586818b
Fabien Crespel <fabien@crespel.net>
2016-08-22 00:40:56 +0200

    CAS: add management webapp to admin tools (refs #4530)

---
+++ admin/cas-management/src/main/product/data/services/portal/admintool-cas-management.inc.php
@@ -0,0 +1,16 @@
+<?php
+// CAS Management admin tool definition
+return array(
+	'weight'		=> 5,
+	'id'			=> 'cas-management',
+	'url'			=> '/admin/cas-management/',
+	'title'			=> 'CAS',
+	'description'	=> "Service central d'authentification",
+	'details'		=> <<<EOL
+        <ul>
+          <li>Gestion de <strong>l'authentification des services</strong> de la plateforme</li>
+          <li><em>Plus d'informations : <a href="https://apereo.github.io/cas/">site officiel</a></em></li>
+        </ul>
+EOL
+
+);
