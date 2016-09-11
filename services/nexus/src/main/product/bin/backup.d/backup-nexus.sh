0e548d0baef552cf986ba1d69b64cc96efcdc954
Fabien Crespel <fabien@crespel.net>
2016-09-11 15:37:03 +0200

    Nexus: exclude trash directory from backup (refs #3587)

--- services/nexus/src/main/product/bin/backup.d/backup-nexus.sh
+++ services/nexus/src/main/product/bin/backup.d/backup-nexus.sh
@@ -8,5 +8,5 @@ SOURCE_DIR="@{package.data}/"
 TARGET_DIR="@{package.backup}/"
 
 if [ -d "$SOURCE_DIR" -a -d "$TARGET_DIR" ]; then
-	rsync -rthvzl --delete "$SOURCE_DIR" "$TARGET_DIR"
+	rsync -rthvzl --delete --exclude='.nexus/trash' "$SOURCE_DIR" "$TARGET_DIR"
 fi
