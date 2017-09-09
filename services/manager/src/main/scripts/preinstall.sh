6b3c59891a033c89a81da019fdab596b548db50d
Fabien Crespel <fabien@crespel.net>
2017-09-09 22:40:52 +0200

    Manager: add AJP port monitoring (refs #5824)

--- services/manager/src/main/scripts/preinstall.sh
+++ services/manager/src/main/scripts/preinstall.sh
@@ -1,6 +1,7 @@
 # Disable Nagios monitoring
 if type -t nagios_disable_service >/dev/null; then
-	nagios_disable_service "Platform Manager"
+	nagios_disable_service "Platform Manager AJP"
+	nagios_disable_service "Platform Manager HTTP"
 fi
 
 # Disable user access to the service
