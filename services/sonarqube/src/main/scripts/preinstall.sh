79a5ebd05b6f33bdf7023d47e68e03adcd3c9c51
Fabien Crespel <fabien@crespel.net>
2017-09-09 14:33:05 +0200

    SonarQube: add AJP port monitoring (refs #5824)

--- services/sonarqube/src/main/scripts/preinstall.sh
+++ services/sonarqube/src/main/scripts/preinstall.sh
@@ -1,6 +1,7 @@
 # Disable Nagios monitoring
 if type -t nagios_disable_service >/dev/null; then
-	nagios_disable_service "SonarQube"
+	nagios_disable_service "SonarQube AJP"
+	nagios_disable_service "SonarQube HTTP"
 fi
 
 # Disable user access to the service
