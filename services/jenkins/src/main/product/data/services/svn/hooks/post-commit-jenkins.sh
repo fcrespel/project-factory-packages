5cbd3c8231e99037bccc459af556b7274a34970e
Fabien Crespel <fabien@crespel.net>
2017-12-19 17:08:24 +0100

    Jenkins: upgrade to version 2.89.2 (refs #6223)
    Also update all plugins, disable JNLP 1/2, enable CSRF crumb issuer

--- services/jenkins/src/main/product/data/services/svn/hooks/post-commit-jenkins.sh
+++ services/jenkins/src/main/product/data/services/svn/hooks/post-commit-jenkins.sh
@@ -7,7 +7,17 @@ REPOS="$1"
 REV="$2"
 UUID=`svnlook uuid $REPOS`
 CHANGES=`svnlook changed --revision $REV $REPOS`
-URL="@{product.scheme}://$PRODUCT_DOMAIN/jenkins/subversion/$UUID/notifyCommit?rev=$REV"
+
+JENKINS_URL="@{product.scheme}://$PRODUCT_DOMAIN/jenkins"
+TOKEN_URL="$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)"
+NOTIFY_URL="$JENKINS_URL/subversion/$UUID/notifyCommit?rev=$REV"
+
+echo "Obtaining CSRF token ..."
+CSRF_HEADER=""
+CSRF_TOKEN=`/usr/bin/wget -q --auth-no-challenge --http-user="$BOT_USER" --http-passwd="$BOT_PASSWORD_MD5" --no-check-certificate --output-document=- "$TOKEN_URL"`
+if [ -n "$CSRF_TOKEN" ]; then
+	CSRF_HEADER="--header $CSRF_TOKEN"
+fi
 
 echo "Notifying Jenkins ..."
 /usr/bin/wget \
@@ -15,10 +25,11 @@ echo "Notifying Jenkins ..."
   --tries=3 \
   --header="Content-Type:text/plain;charset=UTF-8" \
   --post-data="$CHANGES" \
-  --output-document="-" \
+  --output-document=- \
   --auth-no-challenge \
-  --http-user=$BOT_USER \
-  --http-passwd=$BOT_PASSWORD_MD5 \
+  --http-user="$BOT_USER" \
+  --http-passwd="$BOT_PASSWORD_MD5" \
   --no-check-certificate \
-  $URL
+  $CSRF_HEADER \
+  "$NOTIFY_URL"
 
