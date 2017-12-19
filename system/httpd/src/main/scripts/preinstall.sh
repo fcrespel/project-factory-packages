763335e576386e9cf431b64595437032db920c7e
Fabien Crespel <fabien@crespel.net>
2017-12-19 17:54:18 +0100

    HTTPD: fix vhosts config in preinstall

--- system/httpd/src/main/scripts/preinstall.sh
+++ system/httpd/src/main/scripts/preinstall.sh
@@ -1,7 +1,7 @@
 # Create the vhosts directory if necessary
-if [ ! -e "@{system.httpd.app.vhosts}" ]; then
-	mkdir -p "@{system.httpd.app.vhosts}"
-	if [ ! -e "@{system.httpd.app.conf}/vhosts.conf" ] && ! grep -q -E -r --include='*.conf' '^\s*Include @{system.httpd.app.vhosts}/\*\.conf' "@{system.httpd.app}" >/dev/null 2>&1; then
-		echo 'Include @{system.httpd.app.vhosts}/*.conf' > "@{system.httpd.app.conf}/vhosts.conf"
-	fi
+mkdir -p "@{system.httpd.app.vhosts}"
+
+# Include vhosts config files if necessary
+if [ ! -e "@{system.httpd.app.conf}/vhosts.conf" ] && ! grep -q -E -r --include='*.conf' '^\s*Include @{system.httpd.app.vhosts}/\*\.conf' "@{system.httpd.app}" >/dev/null 2>&1; then
+	echo 'Include @{system.httpd.app.vhosts}/*.conf' > "@{system.httpd.app.conf}/vhosts.conf"
 fi
