63c14c07087a402a0e552ee96647a48d6a23c18c
Fabien Crespel <fabien@crespel.net>
2018-04-07 19:45:04 +0200

    PHP: purge sessions in postinstall

--- system/php/src/main/scripts/postinstall.sh
+++ system/php/src/main/scripts/postinstall.sh
@@ -8,6 +8,9 @@ chown @{product.user}:@{product.group} "$PEAR_CFG"
 # Extract Composer binaries
 tar -C "@{package.app}/composer/vendor" -xf "@{package.app}/composer/vendor/bin.tar"
 
+# Purge sessions
+find "@{package.data}/sessions" -name 'sess_*' -delete
+
 # Fix file and directory permissions
 chown -R @{package.user}:@{package.group} "@{package.app}" "@{package.data}/sessions"
 chmod -R ug=rwX "@{package.data}/sessions"
