d94555066793e4e563592a127580c29b7bed26b2
Fabien Crespel <fabien@crespel.net>
2018-04-08 20:27:22 +0200

    HTTPD: fix preinstall for Debian-based distros

--- system/httpd/src/main/scripts/preinstall.sh
+++ system/httpd/src/main/scripts/preinstall.sh
@@ -2,6 +2,8 @@
 mkdir -p "@{system.httpd.app.vhosts}"
 
 # Include vhosts config files if necessary
-if [ ! -e "@{system.httpd.app.conf}/vhosts.conf" ] && ! grep -q -E -r --include='*.conf' '^\s*Include @{system.httpd.app.vhosts}/\*\.conf' "@{system.httpd.app}" >/dev/null 2>&1; then
-	echo 'Include @{system.httpd.app.vhosts}/*.conf' > "@{system.httpd.app.conf}/vhosts.conf"
+if [ -z "@{system.httpd.app.vhosts.enabled}" ]; then
+	if [ ! -e "@{system.httpd.app.conf}/vhosts.conf" ] && ! grep -q -E -r --include='*.conf' '^\s*Include(Optional)? @{system.httpd.app.vhosts}/\*\.conf' "@{system.httpd.app}" >/dev/null 2>&1; then
+		echo 'Include @{system.httpd.app.vhosts}/*.conf' > "@{system.httpd.app.conf}/vhosts.conf"
+	fi
 fi
