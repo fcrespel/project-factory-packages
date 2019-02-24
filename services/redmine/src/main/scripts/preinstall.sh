52af962d59af0eb23f85ef22ee541db74a61c9d7
Fabien Crespel <fabien@crespel.net>
2019-02-24 18:13:31 +0100

    Redmine: update to version 3.4.9

--- services/redmine/src/main/scripts/preinstall.sh
+++ services/redmine/src/main/scripts/preinstall.sh
@@ -1,9 +1,10 @@
 # Disable cron jobs
-touch "@{product.bin}/cron.5mins/redmine-jenkins-sync.sh.lock"
-touch "@{product.bin}/cron.5mins/redmine-ldap-sync.sh.lock"
-touch "@{product.bin}/cron.daily/redmine-reminders.sh.lock"
-touch "@{product.bin}/cron.daily/redmine-attachments-prune.sh.lock"
-touch "@{product.bin}/cron.hourly/redmine-repo-sync.sh.lock"
+if type -t cron_disable_job >/dev/null; then
+	cron_disable_job "@{product.bin}/cron.5mins/redmine-ldap-sync.sh"
+	cron_disable_job "@{product.bin}/cron.daily/redmine-reminders.sh"
+	cron_disable_job "@{product.bin}/cron.daily/redmine-attachments-prune.sh"
+	cron_disable_job "@{product.bin}/cron.hourly/redmine-repo-sync.sh"
+fi
 
 # Disable Nagios monitoring
 if type -t nagios_disable_service >/dev/null; then
