0af4b11255c4e451fb80bb04cadc5f528fb23855
Fabien Crespel <fabien@crespel.net>
2019-06-25 23:49:48 +0200

    Ruby: support multiple versions, starting with 2.4.6/2.5.5 (refs #6742)

--- system/ruby/src/main/product/bin/functions.ruby.sh
+++ system/ruby/src/main/product/bin/functions.ruby.sh
@@ -23,14 +23,15 @@ function rvm_quiet
 # Install Ruby gems from a local source
 function installgems
 {
-	local RUBY_GEMS="$1"
+	local RUBY_VERSION="$1"
+	local RUBY_GEMS="$2"
 	local RUBY_GEMS_DIR="@{package.app}/gems"
 	for RUBY_GEM in $RUBY_GEMS; do
 		RUYGEM_OUTPUT=`mktemp --tmpdir=$PRODUCT_TMP`
-		if ! rvm default do gem install --local $RUBY_GEMS_DIR/$RUBY_GEM.gem --no-ri --no-rdoc > "$RUYGEM_OUTPUT" 2>&1; then
+		if ! rvm $RUBY_VERSION do gem install --local $RUBY_GEMS_DIR/$RUBY_GEM.gem --no-document > "$RUYGEM_OUTPUT" 2>&1; then
 			cat "$RUYGEM_OUTPUT"
 			rm -f "$RUYGEM_OUTPUT"
-			printerror "ERROR: failed to install Ruby gem '$RUBY_GEM'"
+			printerror "ERROR: failed to install Ruby gem '$RUBY_GEM' for Ruby version $RUBY_VERSION"
 			return 1
 		fi
 		rm -f "$RUYGEM_OUTPUT"
